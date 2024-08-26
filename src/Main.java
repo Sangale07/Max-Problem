596d74d86b7c217dd00dc9e4e3d4b613a0bdb7cb
rushi <rushikeshsangale57@gmail.com>
2024-08-26 11:55:15 +0530

    Float Program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(maxFloat(10.2f,25.2f,30.1f));
+    }
+
+    static float maxFloat(float a, float b , float c){
+        if(a > b && a > c){
+            return a;
+        }
+        else if(b > a && b > c){
+            return b;
+        }
+        else {
+            return c;
+        }
     }
 }
