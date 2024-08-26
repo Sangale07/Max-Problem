72c41e4379928286d780722925f6ecc3e564321a
rushi <rushikeshsangale57@gmail.com>
2024-08-26 13:29:42 +0530

    Finding max element from more than three variables

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr =  new int[n];
+        for(int  i = 0 ; i< n ; i++){
+            arr[0] = sc.nextInt();
+        }
 
         Compare<String> c1 = new Compare<>("Banana" , "Peach" , "Apple");
         Compare<Integer> c = new Compare<>(10, 15, 20);
@@ -11,6 +18,12 @@ public class Main {
         System.out.println(c1.compare(c1.a, c1.b,c1.c));
         System.out.println(c.compare(c.a, c.b, c.c));
         System.out.println(c.compare(c2.a, c2.b, c2.c));
+        System.out.println(max(arr , n));
+    }
+    static int max(int[] arr , int n){
+        Arrays.sort(arr);
+        return arr[n - 1];
+
     }
 }
 
