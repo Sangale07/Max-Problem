444fdabdd1b70e1f0d62d4a74abb0c752d6aee02
rushi <rushikeshsangale57@gmail.com>
2024-08-26 11:49:38 +0530

    Max Integer Program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(maxint(10,25,12));
+    }
+    static int maxint(int a , int b , int c){
+        int max = 0;
+        max = Math.max(a , b);
+        max = Math.max(max , c);
+
+        return max;
     }
 }
